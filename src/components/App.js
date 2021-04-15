39d47aba4c4bdc1ec397a23f59ad69a6f88407b4
solda <solda@khu.ac.kr>
2021-04-15 15:36:25 +0900

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import SolRouter from "./Router";
 
 function App() {
-  return <div></div>;
+  return <SolRouter />;
 }
 
 export default App;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,3 +1,7 @@
 import React from "react";
 
-export default()=><span>Auth</span>
+// export default () => <span>Auth</span>
+
+// 자동 import
+const Auth = () => <span>Auth</span>;
+export default Auth;
